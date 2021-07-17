a9bc8d63959dfe3fdffec16b137189d3b16c5a98
alanrs2020 <alanrs2020@gmail.com>
2021-07-17 15:25:34 +0530

    release

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${getUniqueId()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg,roomId) => {
+    console.log('message: ' + msg+roomId);
+    io.emit('chat message', msg, roomId);
+  });
+});
+server.listen(3000)
+function getUniqueId() {
+  // body...
+
+    var length = 10;
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+  return result;
+}
